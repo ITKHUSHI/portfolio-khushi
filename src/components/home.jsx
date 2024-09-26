8041efa78d598178975b70cd20d3bde596b08d8c
ITKHUSHI <khushirathore252002@gmail.com>
2024-09-26 15:25:00 +0530

    adding Mern Stack project details

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,17 +6,16 @@ import { TextGenerateEffect } from "../components/ui/text-generate-effect.jsx";
  
 
 const Home = () => {
-  const words=`I'm khushi a skilled frontend developer with a passion for crafting captivating web experiences. With expertise in various frameworks and libraries, I specialize in creating visually stunning websites that leave a lasting impression.`;
+  const words=`Hello, I'm Khushi Rathore! 👋`;
   
 
   return (
     <>
   
 
-  <main className=' bg-black shadow-xl  w-full  h-100vh sm:h-[680px] '>
+  <main className=' bg-black shadow-xl  w-full '>
  
-  <div className="flex flex-col  sm:flex-row items-center justify-center sm:p-8 sm:justify-center sm:items-center  ">
-      {/* { Image Section} */}
+  <header className="flex flex-col   md:flex-row items-center justify-center p-8   ">
        
       <div className="order-1  mt-16  rounded-full  px-4 mb-4 md:mb- 0  items-center justify-center flex  ">
         <img
@@ -27,65 +26,23 @@ const Home = () => {
         />
        
       </div>
-        
-      {/* About Section */}
-      <div className="order-2 md:order-2 w-full md:w-1/2 px-4 p-2 sm:mt-16  text-white  justify-center items-center  ">
+
+          <div className=" md:order-2 w-full  px-4 p-2 sm:mt-16  text-white  justify-center items-center  ">
           <div className='ontent p-4  border border-black border-radius-custom flex flex-col justify-start items-start '>
           
         <span className="mb-4 text-lg leading-relaxed   "> 
         <TextGenerateEffect words={words} />
+        <p>I'm an aspiring Software Engineer with a passion for building innovative, user-friendly applications. Aligned with the latest web technologies, I specialize in MERN Stack development, crafting both frontend and backend solutions. My journey revolves around transforming ideas into functional, beautiful digital experiences, whether it's designing seamless user interfaces or building robust, scalable server-side applications.
+        Let's create something amazing together!</p>
 </span>
        
           </div>
           
       </div>
       
-    </div>
-   <div >
-   <ul  className=" items-center justify-center flex p-4 gap-4"> 
-        <li className=' hover:scale-100 scale-75 flex  w-[35px]  h-[35px] bg-gradient-to-tr from-red-700 to-orange-500 shadow-lg shadow-orange-500  rounded-full text-white justify-center items-center '>
-        <Link to="https://www.instagram.com/accounts/onetap/?next=%2F"
-        className=' '>
-      <svg xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 448 512"
-      className='  w-[30px] h-[30px]  text-white'>
-        
-      <path fill="#f7f9fd" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-      </svg>
-      </Link>
-
-        </li>
-      
-        <li className=' hover:scale-100 scale-75 flex justify-center items-center w-[35px] h-[35px] bg-gradient-to-tr from-black to-green-500 rounded-full shadow-lg  shadow-green-500 text-white text-center'>
-           <Link to="https://mail.google.com/mail/u/2/?ogbl#inbox">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-        className='  w-[25px] h-[25px]  text-white'
-        ><path fill="#f7f9fd" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-       
-        </Link>
-         </li >
-        <li className= 'hover:scale-100 scale-75 flex justify-center items-center w-[35px] h-[35px] bg-gradient-to-tr from-blue-700 to-blue-500   shadow-blue-500 shadow-lg rounded-full text-white text-center'>
-           <Link to="https://twitter.com/__Khushi__i">
-           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
-             className='w-[27px] h-[27px] text-white '
-           ><path fill="#f7f9fd" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
-        </Link>
-        </li >
-        <li className=' hover:scale-100 scale-75 flex justify-center items-center w-[35px] h-[35px] bg-gradient-to-tr from-green-700 to-green-500 rounded-full shadow-lg  shadow-green-500 text-white text-center'> 
-        <Link to="http://Wa.me/+916263505349" >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-         className='  w-[30px] h-[30px]  '
-        ><path fill="#f7f9fd" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/>
-        </svg>
-        </Link></ li >
-        <li className=' flex justify-center items-center w-[35px] h-[35px] hover:scale-100 scale-75 bg-gradient-to-tr  from-blue-700 to-blue-500 rounded-full shadow-lg  shadow-blue-500 text-white text-center'>
-           <Link to="https://www.linkedin.com/in/khushee0225/" >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='w-[30px] h-[30px] rounded-full' ><path  fill="#f7f9fd" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-        </Link></ li >
-      </ul>
-      
-      
-      <SparklesCore
+    </header>
+   
+   <SparklesCore
           id="tsparticlesfullpage"
           background="transparent"
           minSize={1.6}
@@ -94,11 +51,78 @@ const Home = () => {
           className="w-full h-full"
           particleColor="#FFFFFF"
         />  
-   </div>
+   
    </main>
+    
+    <section className='bg-black text-white justify-center items-center flex flex-col w-full ml-12'>
+        <h2 className='text-center font-bold  text-2xl'>VidStream Hub</h2>
+    
+     <div className='w-full justify-evenly flex flex-col md:flex-row'>
+        <div className=' md:w-[50%] p-4 '>
+        <div className=' align-center justify-center flex flex-col ' >
+
+               <p>Project Overview: VidStream Hub is a full-featured video-sharing platform built using the MERN Stack (MongoDB, Express, React, Node.js). It allows users to seamlessly log in through Google or Gmail authentication, upload their own videos, watch videos uploaded by others, interact through likes and comments, and subscribe to content channels they enjoy. This platform empowers content creators and viewers to engage, share, and explore diverse video content in a modern and intuitive environment.</p>
+                <img src="./images/videoProject1.png" alt="" className='p-2 w-[80%] '  />
+                <p>
+                <br />
+State Management: Powered by Redux, ensuring smooth state handling across the app.
+<br />
+API Integration: All backend API interactions are managed through Axios.
+<br />
+Stylish UI: The user interface is designed using Tailwind CSS, offering a sleek, modern look and mobile responsiveness.
+
+                </p>
+               </div>
+        </div>
+        <div className=' md:w-[50%] align-center justify-center flex flex-col md:flex-row' >
+               <div className='w-[70%]  p-4 '>
+               <img src="./images/videoProject1.png" alt="" className='w-[80%] m-4'  />
+                 <p>
+
+Key Features:
+<br />
+User Authentication: Secure login via Google or Gmail, simplifying the user experience with one-click authentication.
+<br />
+Video Uploading: Users can upload their own videos, with file uploads managed through Multer.
+<br />
+Video Interaction: Viewers can watch videos, like them, comment on them, and subscribe to specific channels for updates on new content.
+<br />
+Content Feed: Explore uploaded videos by other users, enabling content discovery.
+<br />
+Real-time Notifications: Utilizes React-hot-toast for instant feedback on actions like uploads, comments, and subscriptions.
+</p>
+               </div>
+        </div>
+      </div> 
+      <div className='flex flex-wrap justify-center items-center md:flex-col'> 
+     <h1 className='font-bold text-xl'> Technologies Used:</h1>  
+
+MongoDB for database management
+<br />
+Express for server-side routing and middleware handling
+<br />
+Node.js for backend runtime environment
+<br />
+Multer for handling file uploads (video files)
+<br />
+React for building the frontend UI
+<br />
+React-hot-toast for notification handling
+<br />
+Tailwind CSS for styling and responsive design
+<br />
+Axios for API requests
+<br />
+Redux for global state management
+<br />
+<br/>
+ <strong className='font-bold text-xl'>Current Status:</strong>   VidStream Hub is still a work in progress, with some features under development. While it's not deployed yet, you can view the project's progress and code via my portfolio.
+
+
+      </div>
+    </section>
   
-  
-    <section className="text-white bg-black p-6 flex justify-center items-center  ">
+    <footer className="text-white bg-black p-6 flex justify-center items-center">
         <div className="bg-black mt-24 flex justify-center  sm:justify-between items-center  lg:mt-0 flex-col  ">
 
         <div className='flex justify-between items-center  gap-4 lg:gap-8'>
@@ -141,10 +165,26 @@ const Home = () => {
             </div>
             </div>
      
+            <div >
+   <ul  className=" items-center justify-start flex p-2 m-4 gap-4">       
+        <li className=' hover:scale-100 scale-75 flex justify-center items-center w-[35px] h-[35px] bg-gradient-to-tr from-black to-green-500 rounded-full shadow-lg  shadow-green-500 text-white text-center'>
+           <Link to="https://mail.google.com/mail/u/2/?ogbl#inbox">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"
+        className='  w-[25px] h-[25px]  text-white'
+        ><path fill="#f7f9fd" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+       
+        </Link>
+         </li > 
+        <li className=' flex justify-center items-center w-[35px] h-[35px] hover:scale-100 scale-75 bg-gradient-to-tr  from-blue-700 to-blue-500 rounded-full shadow-lg  shadow-blue-500 text-white text-center'>
+           <Link to="https://www.linkedin.com/in/khushee0225/" >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='w-[30px] h-[30px] rounded-full' ><path  fill="#f7f9fd" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+        </Link></ li >
+      </ul>
+   </div>
         
             
         </div>
-      </section>
+      </footer>
  
   
  
