6908b2d2885939d5a310ed0bc2b8f5e55a303366
khushi Rathore <122520330+ITKHUSHI@users.noreply.github.com>
2025-11-01 15:34:52 +0530

    Update certificate image URL for Gen AI Academy

--- src/data/cerificates.data.js
+++ src/data/cerificates.data.js
@@ -3,7 +3,7 @@ const certificates = [
     name: "Gen AI Academy Completion Certificate",
     date: "Jun 2025",
     organization: "Google Cloud",
-	img:"https://media.licdn.com/dms/image/v2/D562DAQE_c4n3-CbB3Q/profile-treasury-image-shrink_800_800/B56ZdaJt.tGoAY-/0/1749564189225?e=1760698800&v=beta&t=SvEgKSlQmxdkgcG7rhGSK3mtm06QsUz-x8ewgce9YSQ"
+	img:"https://certificate.hack2skill.com/user/genai3/2025H2S04GENAI-A300107?trk=public_profile_see-credential"
   },
   {
     name: "Build Real World AI Applications with Gemini and Imagen",
